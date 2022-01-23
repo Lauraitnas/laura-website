88beb85ca68750e57050f9f4b5c282dc8ce545fe
Laura Santi <89958677+Lauraitnas@users.noreply.github.com>
2022-01-23 14:29:55 +0800

    Website finished!

--- effect.js
+++ effect.js
@@ -1,34 +1,8 @@
+const btn = document.querySelector('.btn');
 
-// const button = document.querySelector('.button');
-
-// button.addEventListener('click', () => {
-//     let overlay = document.createElement('span');
-//     overlay.style.cssText = "
-//     position: absolute;
-//     height: 200px;
-//     width: 200px;
-//     top: 50%;
-//     left: 50%;
-//     transform: translate(-50%, -50%);
-//     background-color: #22646e;
-//     z-index: -1;
-//     border-radius: 50%;
-//     opacity: 0;
-//     animation= button_animation 1s ease;
-//     "
-
-//     button.appendChild(overlay)
-// })
-
-const buttons = document.querySelector('.button');
-
-buttons.forEach(btn => {
-    btn.addEventListener('click', e => {
-        let xAxis = e.clientX - e.target.offSetLeft
-        let yAxis = e.clientY - e.target.offSetTop
-
-        let span = document.createElement('span');
-        span.style.left = $ {xAxis}px
-
-    })
-})
+    btn.onmousemove = function(e){
+        const x = e.pageX - btn.offSetLeft;
+        const y = e.pageY - btn.offSetTop;
+        btn.style.setProperty('--x', x + 'px');
+        btn.style.setProperty('--y', y + 'px');
+    }
